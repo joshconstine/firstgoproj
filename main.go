646288069ded26edb80847c2a2cc19465fe60639
joshua constine <joshua.constine97@gmail.com>
2023-09-30 11:38:32 -0700

    added html to main path

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 
 func main() {
+	port := 8080
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Set the Content-Type header to indicate that we are sending HTML.
+		w.Header().Set("Content-Type", "text/html")
+
+		// Write your HTML content to the response writer.
+		fmt.Fprintf(w, "<html><body><h1>Hello, Go HTML Server!</h1></body></html>")
+	})
+	
+	fmt.Printf("Server is listening on port %d...\n", port)
 	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello World"))
 	})
