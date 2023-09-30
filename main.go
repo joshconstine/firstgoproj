be630c2af20e5a5f183e71dd0bbd661171785f0c
joshua constine <joshua.constine97@gmail.com>
2023-09-30 15:51:31 -0700

    added more routes and added gorilla

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"strconv"
 	"net/http"
+	"os"
+	"io"
+    "github.com/gorilla/mux"
 )
 
 
@@ -11,17 +14,65 @@ func main() {
 	port := 8080
 	// Convert the integer port to a string.
 	portStr := strconv.Itoa(port)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Set the Content-Type header to indicate that we are sending HTML.
-		w.Header().Set("Content-Type", "text/html")
 
-		// Write your HTML content to the response writer.
-		fmt.Fprintf(w, "<html><body><h1>Hello, Go HTML Server!</h1></body></html>")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		htmlFile, err := os.Open("public/index.html")
+		if err != nil {
+			http.Error(w, "Unable to read HTML file", http.StatusInternalServerError)
+			return
+		}
+		defer htmlFile.Close()
+
+		// Copy the HTML content to the response writer.
+		_, err = io.Copy(w, htmlFile)
+		if err != nil {
+			http.Error(w, "Unable to copy HTML content to response", http.StatusInternalServerError)
+			return
+		}
+	})
+	r.HandleFunc("/recipes", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		htmlFile, err := os.Open("public/recipes.html")
+		if err != nil {
+			http.Error(w, "Unable to read HTML file", http.StatusInternalServerError)
+			return
+		}
+		defer htmlFile.Close()
+
+		// Copy the HTML content to the response writer.
+		_, err = io.Copy(w, htmlFile)
+		if err != nil {
+			http.Error(w, "Unable to copy HTML content to response", http.StatusInternalServerError)
+			return
+		}
+	})
+	r.HandleFunc("/recipes/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+        id := vars["id"]
+		fmt.Fprintf(w, "You've requested the recipe: %s\n", id)
+
+		// w.Header().Set("Content-Type", "text/html")
+		// htmlFile, err := os.Open("public/recipes.html")
+		// if err != nil {
+		// 	http.Error(w, "Unable to read HTML file", http.StatusInternalServerError)
+		// 	return
+		// }
+		// defer htmlFile.Close()
+
+		// // Copy the HTML content to the response writer.
+		// _, err = io.Copy(w, htmlFile)
+		// if err != nil {
+		// 	http.Error(w, "Unable to copy HTML content to response", http.StatusInternalServerError)
+		// 	return
+		// }
 	})
 	
 	fmt.Printf("Server is listening on port %d...\n", port)
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello World"))
 	})
-	http.ListenAndServe(":"+portStr, nil)
+	http.ListenAndServe(":"+portStr, r)
 }
