a1a48db92969f459b75f19c383186af94626452d
joshua constine <joshua.constine97@gmail.com>
2023-10-09 11:43:25 -0700

    uploading to s3

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.19
 require github.com/gorilla/mux v1.8.0
 
 require (
+	github.com/aws/aws-sdk-go v1.45.24 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/uuid v1.3.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/twilio/twilio-go v1.14.0 // indirect
--- main.go
+++ main.go
@@ -4,20 +4,45 @@ import (
 	"fmt"
 	"os"
 	"io"
-	"path/filepath"
+	"io/ioutil"
 
+	"path/filepath"
+	"log"
+	"bytes"
 	"github.com/google/uuid"
 	"strconv"
+	"context"
+
 	"net/http"
     "github.com/gorilla/mux"
     "firstgoprog/api" // Replace "firstgoprog" with your actual module name.
 	"database/sql"
     _ "github.com/go-sql-driver/mysql"
     _ "github.com/joho/godotenv/autoload"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/credentials"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 
-func UploadHandler(w http.ResponseWriter, r *http.Request) {
+func NewUploader() *s3manager.Uploader {
+	ACCESS_KEY:= "AKIAX6ZNEPNPAR6OXRRO"
+	SECRET_KEY:= "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"
+	s3Config := &aws.Config{
+		Region:      aws.String("us-west-1"),
+		Credentials: credentials.NewStaticCredentials(ACCESS_KEY, SECRET_KEY, ""),
+	}
+
+	s3Session := session.New(s3Config)
+
+	uploader := s3manager.NewUploader(s3Session)
+	fmt.Printf("Created new S3 Uploder")
+	
+	return uploader
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request, uploader *s3manager.Uploader) {
 	err := r.ParseMultipartForm(10 * 1024 * 1024) // 10 MB limit
 	if err != nil {
 		http.Error(w, "Failed to parse multipart form", http.StatusInternalServerError)
@@ -50,6 +75,37 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to copy the file", http.StatusInternalServerError)
 		return
 	}
+	log.Println("uploading so S3")
+
+	// file, err := ioutil.ReadFile(newFilePath)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+
+BUCKET_NAME := "foodly-bucket"
+// BUCKET_URL := "https://foodly-bucket.s3.us-west-1.amazonaws.com"
+// NEXT_PUBLIC_BUCKET_URL := "https://foodly-bucket.s3.us-west-1.amazonaws.com"
+
+//** try with copy
+fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Failed to read file to bytes", http.StatusInternalServerError)
+
+		return 
+	}
+
+
+
+	upInput := &s3manager.UploadInput{
+		Bucket:      aws.String(BUCKET_NAME), // bucket's name
+		Key:         aws.String(newFilePath),        // files destination location
+		Body:        bytes.NewReader(fileBytes),                   // content of the file
+		ContentType: aws.String(fileExt),                 // content type
+	}
+	res, err := uploader.UploadWithContext(context.Background(), upInput)
+	log.Printf("res %+v\n", res)
+	log.Printf("err %+v\n", err)
 
 	// Respond with the unique filename or other relevant information
 	fmt.Fprintf(w, "File uploaded successfully with filename: %s", newFilename)
@@ -60,8 +116,14 @@ func main() {
 	// Convert the integer port to a string.
 	portStr := strconv.Itoa(port)
 	
+	uploader := NewUploader()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/upload", UploadHandler)
+	// r.HandleFunc("/upload", UploadHandler(uploader))
+	r.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+        UploadHandler(w, r, uploader)
+    }).Methods("POST")	
+
 
 	db, err := sql.Open("mysql", "root:daddy@(db:3306)/food?parseTime=true")
 	
