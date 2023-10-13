7fe0a30d281df38c8852c90c8352aba2e8c4197b
joshua constine <joshua.constine97@gmail.com>
2023-10-12 17:42:31 -0700

    list is showing quantities

--- api/list.go
+++ api/list.go
@@ -20,6 +20,13 @@ type ListPageData struct {
     Ingredients []Ingredient
 }
 
+type IngredientQuantityData struct {
+	IngredientName string
+    Quantity float32
+    QuantityTypeName string
+    IngredientId int
+}
+
 //HTML TEMPLATES
 
 func GetListTemplate(w http.ResponseWriter, r *http.Request, db *sql.DB) {
@@ -58,7 +65,7 @@ func GetGenerateListTemplate(w http.ResponseWriter, r *http.Request, db *sql.DB)
 			}
 	
 	rows, err := db.Query(`
-    SELECT i.name, ri.quantity, qt.name
+    SELECT i.name, ri.quantity, qt.name, i.ingredient_id
     FROM ingredients i
     INNER JOIN recipe_ingredients ri ON i.ingredient_id = ri.ingredient_id
     INNER JOIN quantity_type qt ON ri.quantity_type_id = qt.quantity_type_id
@@ -71,45 +78,64 @@ if err != nil {
 }
 defer rows.Close()
 
+var ingredientQuantityData []IngredientQuantityData
+
+
 // Loop through the rows of ingredients and append them to the list
 for rows.Next() {
     var ingredientName string
     var quantity float32
     var quantityTypeName string
-    err := rows.Scan(&ingredientName, &quantity, &quantityTypeName)
+    var ingredientId int
+    err := rows.Scan(&ingredientName, &quantity, &quantityTypeName, &ingredientId)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+// Create a flag to track if the ingredient with the same ingredientID exists
+ingredientExists := false
 
-    // Convert the float32 to a string with a specific format
-    stringValue := strconv.FormatFloat(float64(quantity), 'f', -1, 32)
-
-    ingredients = append(ingredients, Ingredient{
-        Name: ingredientName + " " + stringValue + " " + quantityTypeName,
+// Iterate through ingredientQuantityData to find a match
+for i, data := range ingredientQuantityData {
+    if data.IngredientId == ingredientId {
+        // Ingredient with the same ID exists, update the quantity
+        ingredientQuantityData[i].Quantity += quantity
+        ingredientExists = true
+        break
+    }else {
+		ingredientExists = false
+	}
+}
+if !ingredientExists {
+    ingredientQuantityData = append(ingredientQuantityData, IngredientQuantityData{
+        IngredientName:    ingredientName,
+        Quantity:          quantity,
+        QuantityTypeName:  quantityTypeName,
+        IngredientId:      ingredientId,
     })
 }
+}
 
-			// Check for errors during rows iteration
-			if err := rows.Err(); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-	
-		// Now, the 'ingredients' slice contains all ingredients from the selected recipes
-		// You can use 'ingredients' as needed, such as displaying them in the response
-		// or performing further processing.
-	
-		tmpl := template.Must(template.ParseFiles("public/list.html"))
-		
+for _, data := range ingredientQuantityData {
+    // Convert the float32 to a string with a specific format
+    stringValue := strconv.FormatFloat(float64(data.Quantity), 'f', -1, 32)
+
+    // Create the formatted ingredient name
+    ingredientName := data.IngredientName + " " + stringValue + " " + data.QuantityTypeName
+
+    // Append the ingredient to the ingredients list
+    ingredients = append(ingredients, Ingredient{
+        Name: ingredientName,
+    })
+}
+		tmpl := template.Must(template.ParseFiles("public/list.html"))	
 		data := ListPageData{
 			PageTitle: "Your List",
             Ingredients: ingredients,
-        }
-
+		}
         tmpl.Execute(w, data)
 	}
+}
 	
 
 	//DB transactions
