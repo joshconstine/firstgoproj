f5d7336a4ea91fffcda3c550945f089a464b7030
joshua constine <joshua.constine97@gmail.com>
2023-11-08 19:15:00 -0800

    added get tags json endpoint

--- api/routes.go
+++ api/routes.go
@@ -228,6 +228,10 @@ apiRouter.HandleFunc("/favorite", SessionMiddleware( func(w http.ResponseWriter,
     apiRouter.HandleFunc("/mobile/v1/ingredients",  func(w http.ResponseWriter, r *http.Request) {
         GetIngredientsJSON(w, r, db )
     }).Methods("GET")
+    apiRouter.HandleFunc("/mobile/v1/tags",  func(w http.ResponseWriter, r *http.Request) {
+        GetTagsJSON(w, r, db )
+    }).Methods("GET")
+
     apiRouter.HandleFunc("/mobile/v1/recipes/{id}",  func(w http.ResponseWriter, r *http.Request) {
         GetSingleRecipeJSON(w, r, db )
     }).Methods("GET")
